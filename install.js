0874ef26ebc6acd40cae73cfcc2f3d9953981f81
SAMRAT69 <157216723+SAMRAT69@users.noreply.github.com>
2025-03-15 13:36:15 +0545

    Create install.js

---
+++ install.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Sammy Bypass
+// @homepageURL  https://discord.com/invite/u8fDraATDW
+// @namespace    https://greasyfork.org/en/scripts/493960-arceus-bypass
+// @version      1.8
+// @description  bypass many links
+// @author       Samrat
+// @match        https://linkvertise.com/376138/arceus-x-neo-key-system-1?o=sharing*
+// @match        https://linkvertise.com/*
+// @match        https://socialwolvez.com/*
+// @match        https://rekonise.com/*
+// @match        https://mboost.me/a/*
+// @match        https://loot-link.com/s?*
+// @match        https://loot-links.com/s?*
+// @match        https://lootlink.org/s?*
+// @match        https://lootlinks.co/s?*
+// @match        https://gateway.platoboost.com/a/2569?id=*
+// @match        https://gateway.platoboost.com/a/8?id=*
+// @match        https://lootdest.info/s?*
+// @match        https://lootdest.org/s?*
+// @match        https://mobile.codex.lol/*
+// @match        https://pandadevelopment.net/getkey?*
+// @match        https://lootdest.com/s?*
+// @match        https://links-loot.com/s?*
+// @match        https://linksloot.net/s?*
+// @match        https://spdmteam.com/key-system*
+// @match        https://*/recaptcha/*
+// @match        https://*.hcaptcha.com/*hcaptcha-challenge*
+// @match        https://*.hcaptcha.com/*checkbox*
+// @match        https://*.hcaptcha.com/*captcha*
+// @require     https://greasyfork.org/scripts/464929-module-jquery-xiaoying/code/module_jquery_XiaoYing.js
+// @require     https://greasyfork.org/scripts/464780-global-module/code/global_module.js
+// @require     https://raw.githubusercontent.com/SAMRAT69/Sammy-Bypass/refs/heads/main/sammybypass.js
+
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_notification
+// @grant        GM_openInTab
+
+// @license      Samrat
+// @connect      linkvertise.com
+// @connect      short-jambo.com
+// @connect      api.codex.lol
+// @connect      cdn.jsdelivr.net
+// @connect      fluxteam.net
+// @connect      spdmteam.com
+// @connect      api-gateway.platoboost.com
+// @connect      api.valyse.best
+// @connect      api.keyrblx.com
+// @connect      pandadevelopment.net
+// @connect      hohohubv-ac90f67762c4.herokuapp.com
+// @connect      tsuo-script.xyz
+// ==/UserScript==
